15fa49c039b6c9223df5c656cd72ec941d21a8a5
David Schott <schottdb@gmail.com>
2015-04-09 15:34:35 -0400

    first commit

---
+++ src/main/java/com/cloudbees/demo/mavendemo/App.java
@@ -0,0 +1,17 @@
+package com.cloudbees.demo.mavendemo;
+
+/**
+ * Demo
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello there.");
+    }
+    
+    public boolean returnsTrue() {
+        return true;
+    }
+
+}
